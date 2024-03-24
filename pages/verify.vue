fb8ca04f5d67808764cf1baa5b079374a6b56418
Odeh Adejoh <stradox@gmail.com>
2024-03-24 12:38:25 +0100

    ditto last commit

---
+++ pages/verify.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['is-authenticated'],
+  layout: 'auth-layout',
+});
+
+</script>
+
+
+<template>
+  <section class="py-4">
+    Verify your email
+  </section>
+</template>
