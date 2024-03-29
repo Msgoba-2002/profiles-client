b176d277d9957b0cceacc1df00d260a2497e6173
Odeh Adejoh <stradox@gmail.com>
2024-03-30 00:55:05 +0100

    ditto last commit

---
+++ pages/verify-email/index.client.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth-layout',
+});
+
+const { verifyEmail } = useAuthStore();
+const route = useRoute();
+const { token } = route.query;
+const { user } = storeToRefs(useUserStore());
+
+const runVerification = async () => {
+  if (!token) return;
+  try {
+    const data = await verifyEmail(token as string);
+    if (data.statusCode === 200 && user.value) {
+      return navigateTo({ name: 'user-userId', params: { userId: user.value.id } });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+onMounted(() => {
+  runVerification();
+});
+</script>
+
+
+<template>
+  <section class="py-4">
+    <LoadingSpinner loading-label="Working!" />
+  </section>
+</template>
